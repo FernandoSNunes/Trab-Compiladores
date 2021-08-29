37b1c84862b476aa0e53b96281d11403b996afeb
igorlmgx <igorlmgx@gmail.com>
2021-08-28 21:47:40 -0300

    Adicionando comentários do trabalho 1

---
+++ ConstDeCompTrab1/README.md
@@ -0,0 +1,18 @@
+# Trabalho 1
+
+## Analisador léxico com ANTLR em Java
+
+Autores:
+
+|Nome|RA|
+|---|---|
+|Fernando Sassi Nunes | 754754|
+|Gabriel Rodrigues Malaquias | 758906|
+|Igor Lúcio Manta Guedes | 743185|
+
+## Instruções
+
+Antes de executar o projeto, verificar se as versões de Java e JDK estão correspondentes às descritas no arquivo `pom.xml`. Após, basta executar o comando `mvn install` ou compilar utilizando a IDE de prefência.  
+Em seguida, o arquivo compilado estará disponível no caminho `target/lexico-1.0-jar-with-dependencies.jar`.  
+
+##### Nota: existem comentários na classe Principal.java e na gramática Lexers.g4
--- ConstDeCompTrab1/src/main/java/br/ufscar/dc/compiladores/constdecomptrab1/Principal.java
+++ ConstDeCompTrab1/src/main/java/br/ufscar/dc/compiladores/constdecomptrab1/Principal.java
@@ -1,12 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.ufscar.dc.compiladores.constdecomptrab1;
 
 import java.io.IOException;
-import java.io.FileWriter;   // Import the FileWriter class
+import java.io.FileWriter;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.Token;
@@ -21,9 +16,14 @@ public class Principal {
                 FileWriter myWriter = new FileWriter(args[1])) {
                 Lexers lex = new Lexers(cs);
                 Token t = null;
+
+                // interrompe o loop caso seja encontrado um erro
                 boolean continuar = true;
+
                 while ((t = lex.nextToken()).getType() != Token.EOF && continuar) {
                     
+                    // compara o token recebido com os seguintes identificadores
+                    // caso seja encontrado um erro, exibe uma mensagem e encerra o loop
                     if ("ERRO".equals(Lexers.VOCABULARY.getDisplayName(t.getType()))){
                         myWriter.write("Linha " + lex.getLine() + ": " + t.getText() + " - simbolo nao identificado\n");
                         continuar = false;
@@ -48,12 +48,13 @@ public class Principal {
                     else if ("OP_ARIT".equals(Lexers.VOCABULARY.getDisplayName(t.getType()))){
                         myWriter.write("<'" + t.getText() + "','" + t.getText() + "'>\n");
                     }
+                    // caso o token não seja a própria palavra identificada
+                    // ou possua um único identificador definido na gramática g4
                     else
                         myWriter.write("<'" + t.getText() + "'," + Lexers.VOCABULARY.getDisplayName(t.getType()) + ">\n");
                 }
                 myWriter.close();
             }
-        } catch (IOException ex) {
-        }
+        } catch (IOException ex) { }
     }
 }
