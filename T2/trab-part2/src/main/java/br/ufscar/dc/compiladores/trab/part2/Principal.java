1c4a47c4286d7aaae3bf61a10a09e0201d616b72
FernandoSNunes <fer_sassinunes@hotmail.com>
2021-09-11 19:23:28 -0300

    arquivo de saida

--- T2/trab-part2/src/main/java/br/ufscar/dc/compiladores/trab/part2/Principal.java
+++ T2/trab-part2/src/main/java/br/ufscar/dc/compiladores/trab/part2/Principal.java
@@ -1,24 +1,30 @@
-
 package br.ufscar.dc.compiladores.trab.part2;
 
 import java.io.IOException;
+import java.io.FileWriter;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.Token;
 
 public class Principal {
+
     public static void main(String args[]) throws IOException {
         CharStream cs = CharStreams.fromFileName(args[0]);
-        LALexer lexer = new LALexer(cs);
+        AlgumaLexer lexer = new AlgumaLexer(cs);
 
         // Descomentar para depurar o Léxico
         Token t = null;
-        while( (t = lexer.nextToken()).getType() != Token.EOF) {
-            System.out.println("<" + LALexer.VOCABULARY.getDisplayName(t.getType()) + "," + t.getText() + ">");
-       }
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        LAParser parser = new LAParser(tokens);
-        parser.programa();
+        try (
+                // arquivo de saida
+                FileWriter myWriter = new FileWriter(args[1])) {
+            while ((t = lexer.nextToken()).getType() != Token.EOF) {
+                myWriter.write("<" + AlgumaLexer.VOCABULARY.getDisplayName(t.getType()) + "," + t.getText() + ">\n");
+            }
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            AlgumaParser parser = new AlgumaParser(tokens);
+            parser.programa();
+        myWriter.close();
+        }
     }
 }
