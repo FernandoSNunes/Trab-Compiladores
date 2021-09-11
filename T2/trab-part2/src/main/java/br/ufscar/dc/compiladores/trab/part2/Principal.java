d51d9bb8bda0fb5105137eb344944af040d57e58
igorlmgx <igorlmgx@gmail.com>
2021-09-11 19:13:36 -0300

    Alterando nome da gramática

--- T2/trab-part2/src/main/java/br/ufscar/dc/compiladores/trab/part2/Principal.java
+++ T2/trab-part2/src/main/java/br/ufscar/dc/compiladores/trab/part2/Principal.java
@@ -10,15 +10,15 @@ import org.antlr.v4.runtime.Token;
 public class Principal {
     public static void main(String args[]) throws IOException {
         CharStream cs = CharStreams.fromFileName(args[0]);
-        AlgumaLexer lexer = new AlgumaLexer(cs);
+        LALexer lexer = new LALexer(cs);
 
         // Descomentar para depurar o Léxico
         Token t = null;
         while( (t = lexer.nextToken()).getType() != Token.EOF) {
-            System.out.println("<" + AlgumaLexer.VOCABULARY.getDisplayName(t.getType()) + "," + t.getText() + ">");
+            System.out.println("<" + LALexer.VOCABULARY.getDisplayName(t.getType()) + "," + t.getText() + ">");
        }
         CommonTokenStream tokens = new CommonTokenStream(lexer);
-        AlgumaParser parser = new AlgumaParser(tokens);
+        LAParser parser = new LAParser(tokens);
         parser.programa();
     }
 }
